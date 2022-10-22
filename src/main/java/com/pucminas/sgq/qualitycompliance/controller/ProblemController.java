ce5c27cd3c5406bdcb8dd7173f09addd9ba95d4f
Jordyr Caldeira <jordyr.c.v.f@gmail.com>
2022-10-22 13:57:09 -0300

    Adjusting REST path

--- src/main/java/com/pucminas/sgq/qualitycompliance/controller/ProblemController.java
+++ src/main/java/com/pucminas/sgq/qualitycompliance/controller/ProblemController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/compliance/api/v1")
 public class ProblemController {
 
     @Autowired
